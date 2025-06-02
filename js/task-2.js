146908dd51d1f4e2f7b18374f99087eac4037c90
Vitalii Borysiuk <vitalijborysjuk@gmail.com>
2025-06-02 19:12:14 +0300

    js

---
+++ js/task-2.js
@@ -0,0 +1,13 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const onliArray = firstArray.concat(secondArray);
+  return onliArray.length > maxLength
+    ? onliArray.slice(0, maxLength)
+    : onliArray;
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
